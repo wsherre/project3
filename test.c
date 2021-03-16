48f360fe9e9e31e5b315e4ed08ea7d2bf06f22ec
Will Sherrer <wsherre@clemson.edu>
2021-03-15 22:38:53 -0400

    asf

--- test.c
+++ test.c
@@ -8,15 +8,17 @@ int main(int argc, char** argv){
     int *ptr;
     ptr = malloc(5 * sizeof(int));
     for(int i = 0; i < 5; ++i){
-        printf("%d\n", ptr[i]);
+        printf("%d ", ptr[i]);
         ptr[i] = i;
     }
+    printf("\n");
     
 
     
     for(int i = 0; i < 5; ++i){
-        printf("%d\n", ptr[i]);
+        printf("%d ", ptr[i]);
     }
+    printf("\n");
 
     ptr = realloc(ptr, 1 * sizeof(int));
     //ptr[1] = 6;
