a0e7e1ad1ef64690d2b87956339eb7ed6f812ae8
Will Sherrer <wsherre@clemson.edu>
2021-03-15 22:33:54 -0400

    adsf

--- test.c
+++ test.c
@@ -6,16 +6,16 @@
 int main(int argc, char** argv){
 
     int *ptr;
-    ptr = calloc(1, sizeof(int));
+    ptr = calloc(5, sizeof(int));
     printf("%d\n", *ptr);
 
     *ptr = 5;
     printf("%d\n", *ptr);
 
-    ptr = realloc(ptr, 2 * sizeof(int));
-    ptr[1] = 6;
+    ptr = realloc(ptr, 1 * sizeof(int));
+    //ptr[1] = 6;
 
-    printf("%d %d\n", ptr[0], ptr[1]);
+    printf("%d\n", ptr[0]);
     
     free(ptr); 
 }
