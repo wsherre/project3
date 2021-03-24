6f68cc2f4842d00384f3abd698ca4d1b515201ed
Will Sherrer <wsherre@clemson.edu>
2021-03-24 01:26:49 -0400

    adsf

--- allocator.c
+++ allocator.c
@@ -32,7 +32,7 @@ void lib_init(){
         fprintf(stdout, "%p\n", temp);
         int * test = temp + 1;
         fprintf(stdout, "%p\n", test);
-        int t = (int)test | 0x0fff;
+        unsigned int t = (int)test | 0x0fff;
         fprintf(stdout, "%d\n", t);
         *temp = t;
         fprintf(stdout, "%p\t\t%p\t%d\n", temp, test, *test);
