fb007c8089798d7080675dfe280ebfdd444aeebb
Will Sherrer <wsherre@clemson.edu>
2021-03-24 03:07:01 -0400

    asdf

--- test.c
+++ test.c
@@ -6,8 +6,11 @@
 int main(int argc, char** argv){
 
     int* ptr = NULL;
+    int* h = NULL;
     ptr = malloc(5);
+    h = malloc(6);
     *ptr = 16;
-    fprintf(stdout, "%p\t%d\n", ptr, *ptr);
+    *h = 5;
+    fprintf(stdout, "%p\t%d\n%p\t%d\n", ptr, *ptr, h, *h);
     return 0;
 }
