c3fc20125af072a60bdbe5ba2d55f934aed3f747
Will Sherrer <wsherre@clemson.edu>
2021-03-25 01:53:35 -0400

    asdf

--- allocator.c
+++ allocator.c
@@ -4,43 +4,24 @@
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
-void __attribute__((constructor)) lib_init();
-int search(void*);
-void*new_map(int size);
-int find_size(long*);
+
 #define list_size 9
 #define page_size 4096
 
-
+void __attribute__((constructor)) lib_init();
+int search(void*);
+void*new_map(int size);
+void* big_map(int size);
 
 void * map_list[list_size];
-//when doing pointer arithmetic i'm starting at an int and based off of the size of the table changing the multiplier
-int multiplier = 0;
-
 int fd;
 
 
 void lib_init(){
     fd = open ("/dev/zero", O_RDWR ) ;
-    //int * temp;
-    //one for every size except greater than 1k.
     for(int i = 0; i < list_size - 1; ++i){
-        map_list[i] = NULL;
-        /*//multiplier = pow(2, i + 3) / 4;
-        map_list[i] = mmap ( NULL , 4096 , PROT_READ | PROT_WRITE , MAP_PRIVATE , fd , 0) ;
-        temp = map_list[i];
-        *temp = 0;
-        temp++;
-        *temp = (int)NULL;
-        temp+=2;
-        //fprintf(stdout, "%p\n", temp);
-        unsigned int t = (unsigned int)(temp + 1) & 0x00000fff;
-        //fprintf(stdout, "%d\n", t);
-        *temp = t;
-        //fprintf(stdout, "%p\t%d\n", temp, *temp);*/
-        
+        map_list[i] = NULL;  
     }
-    
 }
 
 void * malloc(size_t size){
@@ -93,21 +74,47 @@ void * malloc(size_t size){
         *next_ptr = ptr;
         *(page_start + 4) = ptr;
         return return_ptr;
+    }else{
+        if(map_list[list_size - 1] == NULL){
+            map_list[list_size - 1] = big_map(size);
+            long* page_start =  map_list[list_size - 1];
+            page_start += 2;
+            return page_start;
+        }else{
+            long* page_start =  map_list[list_size - 1];
+            long* next_ptr = (long*)*(page_start + 1);
+
+            while(next_ptr != NULL){
+                page_start = next_ptr;
+                next_ptr = (long*)*(next_ptr + 1);
+            }
+            long* new_block = (long*)big_map(size);
+            *next_ptr = (long)new_block;
+            return (void*)*(next_ptr + 1);
+        }
     }
-    return NULL;
 }
 
 void free(void * ptr){
     long temp = (long)ptr & ~0xfff;
     int* page_start = (int*)temp;
-    long* next_page = (long*)(page_start + 2);
-    int length = *(page_start + 1);
-    *page_start -= (length + 4);
-    int i = log(length)/log(2) - 3;
+    long* next_page;
+    int length, i, big = 0, size;
+    if(*page_start > 4096){
+        big = 1;
+        size = *page_start & 0x7fffffffffffffff;
+    }else{
+        next_page = (long*)(page_start + 2);
+        length = *(page_start + 1);
+        *page_start -= (length + 4);
+        i = log(length)/log(2) - 3;
+        size = page_size;
+    }
 
 
-    if(*page_start == 20 && *next_page == (long)NULL){
-        munmap(page_start, page_size);
+    if( (*page_start == 20 || big) && *next_page == (long)NULL)
+    {
+        munmap(page_start, size);
         if(page_start == map_list[i]){
             map_list[i] = NULL;
         } else{
@@ -120,12 +127,16 @@ void free(void * ptr){
             }
             *(begin_of_page + 1) = (int)NULL;
         }
-    }else if (*page_start == 20 && *next_page != (long)NULL && (void*)page_start == map_list[i])
+    }
+    else if ( (*page_start == 20 || big) && *next_page != (long)NULL && (void*)page_start == map_list[i])
     {
         long* next = (long*)(page_start + 2);
         map_list[i] = (void*)*(next);
-        munmap(page_start, page_size);
-    }else if(*page_start == 20 && *next_page != (long)NULL){
+        munmap(page_start, size);
+    }
+    
+    else if( ( *page_start == 20 || big) && *next_page != (long)NULL)
+    {
         long* begin_of_page = (long*)map_list[i];
         long* next = (long*)*(begin_of_page + 1);
 
@@ -134,7 +145,7 @@ void free(void * ptr){
             next = (long*)*(next + 1);
         }
         *(begin_of_page + 1) = *next_page;
-        munmap(page_start, page_size);
+        munmap(page_start, size);
     }
 }
 
@@ -150,19 +161,32 @@ void * realloc(void * ptr, size_t size){
 
 void* new_map(int v){
         int* temp;
-        void * map = mmap ( NULL , page_size , PROT_READ | PROT_WRITE , MAP_PRIVATE , fd , 0) ;
-        temp = map;
+        void * map = mmap ( NULL , page_size , PROT_READ | PROT_WRITE , MAP_PRIVATE , fd , 0);
+
+        temp = map; 
         *temp = 20;
-        temp++;
+
+        temp++; 
         *temp = v;
-        temp++;
+
+        temp++; 
         *temp = (int)NULL;
+
         temp+=2;
-        //fprintf(stdout, "%p\n", temp);
         unsigned int t = (unsigned int)(temp + 1) & 0x00000fff;
-        //fprintf(stdout, "%d\n", t);
         *temp = t;
-        //fprintf(stdout, "%p\t%d\n", temp, *temp);
 
         return map;
 }
+
+void * big_map(int size){
+    long* temp;
+    void * map = mmap ( NULL , size + 16, PROT_READ | PROT_WRITE , MAP_PRIVATE , fd , 0);
+
+    *temp = (long)size;
+    *temp |= 0x8000000000000000;
+    temp++;
+    *temp = (int)NULL;
+    return map;
+}
+
--- test.c
+++ test.c
@@ -6,13 +6,17 @@
 int main(int argc, char** argv){
 
     int* ptr[679];
-    for(int i = 0; i < 679; ++i){
+    /*for(int i = 0; i < 679; ++i){
         ptr[i] = malloc(8);
         *ptr[i] = i;
         fprintf(stdout, "%p\t%d\n", ptr[i], *ptr[i]);
     }
     for(int i = 678; i >= 0; --i){
         free(ptr[i]);
-    }
+    }*/
+
+    ptr[0] = malloc(1025);
+    ptr[1] = malloc(4093);
+    ptr[2] = malloc(8000);
     return 0;
 }
