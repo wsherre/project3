3543b890db4e848ed737f8dc95e0f90ddfa2c95a
Will Sherrer <wsherre@clemson.edu>
2021-03-25 11:58:59 -0400

    adsf

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv){
 
-    int* ptr[679];
+    int* ptr[100000];
     /*for(int i = 0; i < 679; ++i){
         ptr[i] = malloc(8);
         *ptr[i] = i;
@@ -15,7 +15,7 @@ int main(int argc, char** argv){
         free(ptr[i]);
     }*/
 
-    ptr[0] = malloc(1025);
+    /*ptr[0] = malloc(1025);
     *ptr[0] = 500;
     fprintf(stdout, "%p\t%d\n", ptr[0], *ptr[0]);
     ptr[1] = malloc(4093);
@@ -37,6 +37,17 @@ int main(int argc, char** argv){
     
     free(ptr[1]);
     free(ptr[2]);
-    free(ptr[0]);
+    free(ptr[0]);*/
+
+    for(int i = 4; i < 100000; i++){
+        ptr[i] = malloc(i);
+        *ptr[i] = i;
+    }
+    for(int i = 4; i < 100000; i++){
+        ptr[i] = realloc(ptr[i], i + 1);
+    }
+    for(int i = 4; i < 100000; i++){
+        free(ptr[i]);
+    }
     return 0;
 }
