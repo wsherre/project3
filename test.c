521bbc6c86c52243deb7b6f26bf0662a1b2dcd6d
Will Sherrer <wsherre@clemson.edu>
2021-03-27 14:55:28 -0400

    asdf

--- test.c
+++ test.c
@@ -8,36 +8,14 @@ int main(int argc, char** argv){
     int* ptr[100000];
     
 
-    for(int i = 4; i < 1000; i++){
-        ptr[i] = malloc(i);
-        *ptr[i] = i;
-        fprintf(stdout, "%p\t%d\n", ptr[i], *ptr[i]);
-    }
-    for(int i = 4; i < 1000; i++){
-        ptr[i] = realloc(ptr[i], i + 1);
-        fprintf(stdout, "%p\t%d\n", ptr[i], *ptr[i]);
-    }
-    for(int i = 4; i < 1000; i++){
-        free(ptr[i]);
-    }
+    ptr[0] = malloc(0);
+    for(int i = 4; i < 600; ++i){
+        ptr[0] = realloc(ptr[0], i + 1);
+        ptr[0] = i + 1;
+        fprintf(stdout, "%p\t%d\n", ptr[0], *ptr[0]);
 
-    for( int i = 0; i < 8; ++i){
-        ptr[i] = calloc(1, 527);
-        *ptr[i] = i * 1000;
-        ptr[i] = realloc(ptr[i], 528);
-        fprintf(stdout, "%p\t%d\n", ptr[i], *ptr[i]);
     }
-    for( int i = 0; i < 8; ++i){
-        free(ptr[i]);
-    }
-    for( int i = 4; i < 10000; ++i){
-        ptr[i] = malloc(i);
-        *ptr[i] = i;
-        fprintf(stdout, "%p\t%d\n", ptr[i], *ptr[i]);
-    }
-    for( int i = 4; i < 10000; ++i){
-        free(ptr[i]);
-    }
+    free(ptr[0]);
 
     /*ptr[0] = malloc(1220);
     *ptr[0] = 1220;
