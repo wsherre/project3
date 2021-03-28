26a4cbb7f7b2d8dcf24b3defb8aa4c929a3c94ee
Will Sherrer <wsherre@clemson.edu>
2021-03-28 16:40:36 -0400

    asdf

--- test.c
+++ test.c
@@ -7,15 +7,15 @@ int main(int argc, char** argv){
 
     int* ptr[10000];
 
-    for(int i = 0; i < 10000; ++i){
+    for(int i = 1; i < 10000; ++i){
         ptr[i] = malloc(i);
         fprintf(stdout, "Mallocing %p %d\n", ptr[i], i);
     }
-    for(int i = 1; i < 10000; --i){
+    for(int i = 2; i < 10000; --i){
         free(ptr[i]);
         fprintf(stdout, "Freeing %p %d\n", ptr[i], i);
     }
-    free(ptr[0]);
+    free(ptr[1]);
     ptr[0] = malloc(1220);
     *ptr[0] = 1220;
     ptr[1] = malloc(3000);
