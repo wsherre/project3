dfbbb896e2f77847ec409fad4fd0f8a9b810127a
Will Sherrer <wsherre@clemson.edu>
2021-03-29 15:11:44 -0400

    sadf

--- test.c
+++ test.c
@@ -18,7 +18,7 @@ int main(int argc, char** argv){
         *ptr[i] = i;
         fprintf(stdout, "Reallocing %p %d\n", ptr[i], i);
     }*/
-    for(int i = 1; i > 50000; ++i){
+    for(int i = 1; i < 50000; ++i){
         free(ptr[i]);
         fprintf(stdout, "Freeing %p %d\n", ptr[i], i);
     }
