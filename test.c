3108997b008df69f7265f039969d20bb9e3f7091
Will Sherrer <wsherre@clemson.edu>
2021-03-30 02:43:03 -0400

    asdf

--- test.c
+++ test.c
@@ -7,25 +7,14 @@
 int main(int argc, char** argv){
 
     int* ptr[1000000];
-    //int k = 50000;
+    int k = 50000;
 
 
-    for(int i = 1; i < 4; ++i){
+    for(int i = 1; i < k; ++i){
         ptr[i] = malloc(8);
-        //memset(ptr[i], 5, 4);
         *ptr[i] = i;
         fprintf(stdout, "Mallocing %p %d %d\n", ptr[i], *ptr[i], i);
     }
-    free(ptr[2]);
-    ptr[2] = malloc(8);
-    *ptr[2] = 2;
-    fprintf(stdout, "Mallocing %p %d\n", ptr[2], *ptr[2]);
-    for(int i = 4; i < 10; ++i){
-        ptr[i] = malloc(8);
-        //memset(ptr[i], 5, 4);
-        *ptr[i] = i;
-        fprintf(stdout, "Mallocing %p %d %d\n", ptr[i], *ptr[i], i);
-    }/*
     for(int i = 1; i < k; ++i){
         int num = rand() % 4000;
         ptr[i] = realloc(ptr[i], i);
@@ -35,7 +24,7 @@ int main(int argc, char** argv){
     for(int i = 1; i < k; ++i){
         free(ptr[i]);
         fprintf(stdout, "Freeing %p %d\n", ptr[i], i);
-    }*/
+    }
 
     
     return 0;
