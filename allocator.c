0913be2796013b44d99d3c46ecb6183685b4510e
Will Sherrer <wsherre@clemson.edu>
2021-03-30 00:34:46 -0400

    asdf

--- allocator.c
+++ allocator.c
@@ -202,9 +202,8 @@ void* new_map(int map_page_size){
             *temp = (short)(temp + 1) & 0x0fff;
             temp++;
         }
-        temp += map_page_size/2;
+        temp--;
         *temp = 0;
-        temp = map;
 
         return map;
 }
