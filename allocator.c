118649f608ca2bce2159db0060314c67a005e8a5
Will Sherrer <wsherre@clemson.edu>
2021-03-30 01:07:05 -0400

    asdf

--- allocator.c
+++ allocator.c
@@ -58,7 +58,7 @@ void * malloc(size_t size){
         while(offset == 0){
             if(next_page == NULL){
                 short* new_start = new_map(map_page_size);
-                *next_page = (long) (*((long*)new_start));
+                *next_page = (long) ((long*)new_start);
                 free_list = new_start + 5;
                 offset = *(free_list);
             }else{
