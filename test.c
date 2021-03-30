c017b26882d1659e962e224577f6c547665aab43
Will Sherrer <wsherre@clemson.edu>
2021-03-29 20:26:04 -0400

    asdf

--- test.c
+++ test.c
@@ -10,12 +10,12 @@ int main(int argc, char** argv){
     int k = 50000;
 
     for(int i = 1; i < k; ++i){
-        ptr[i] = malloc(1025);
-        //memset(ptr[i], 5, 8);
+        ptr[i] = malloc(i);
+        //gdbmemset(ptr[i], 5, 8);
         fprintf(stdout, "Mallocing %p %d %d\n", ptr[i], *ptr[i], i);
     }
     for(int i = 1; i < k; ++i){
-        ptr[i] = realloc(ptr[i], 1025);
+        ptr[i] = realloc(ptr[i], i);
         *ptr[i] = i;
         fprintf(stdout, "Reallocing %p %d\n", ptr[i], i);
     }
