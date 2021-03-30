610f83bd80e2e43f28aab3e765b8253b4a2dc2aa
Will Sherrer <wsherre@clemson.edu>
2021-03-30 00:22:03 -0400

    asd

--- test.c
+++ test.c
@@ -11,6 +11,11 @@ int main(int argc, char** argv){
 
     ptr[0] = malloc(8);
     ptr[1] = malloc(8);
+    *ptr[0] = 5;
+    *ptr[1] = 6;
+    fprintf(stdout, "Mallocing %p %d \n", ptr[0], *ptr[0]);
+    fprintf(stdout, "Mallocing %p %d \n", ptr[1], *ptr[1]);
+
     /*for(int i = 1; i < k; ++i){
         ptr[i] = malloc(4000);
         memset(ptr[i], 5, 4);
