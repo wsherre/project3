1f118381f26bde5801142999be6d78f667efc37f
Will Sherrer <wsherre@clemson.edu>
2021-03-30 02:41:39 -0400

    adsf

--- test.c
+++ test.c
@@ -19,7 +19,13 @@ int main(int argc, char** argv){
     free(ptr[2]);
     ptr[2] = malloc(8);
     *ptr[2] = 2;
-    fprintf(stdout, "Mallocing %p %d\n", ptr[2], *ptr[2]);/*
+    fprintf(stdout, "Mallocing %p %d\n", ptr[2], *ptr[2]);
+    for(int i = 4; i < 10; ++i){
+        ptr[i] = malloc(8);
+        //memset(ptr[i], 5, 4);
+        *ptr[i] = i;
+        fprintf(stdout, "Mallocing %p %d %d\n", ptr[i], *ptr[i], i);
+    }/*
     for(int i = 1; i < k; ++i){
         int num = rand() % 4000;
         ptr[i] = realloc(ptr[i], i);
