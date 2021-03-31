7574e19e32c16ffb438257979cfe2bcd722e2cd5
Will Sherrer <wsherre@clemson.edu>
2021-03-31 10:58:02 -0400

    adsf

--- allocator.c
+++ allocator.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define list_size 11
+#define list_size 9
 #define page_size 4096
 #define max_block_size 1024
 /*
@@ -18,7 +18,7 @@ small block headers are like so
 size is the size of the map, 8,16,32,64 etc
 return the pointer by concatinating it with the page head
 
-big block headers are like so
+smabigll block headers are like so
 
 |-----------------|
 | 8 byte size     |
@@ -32,6 +32,7 @@ int search(void*);
 void*new_map(int size);
 void* big_map(int size);
 void* get_free_block(void* head, int map_page_size);
+int return_i(int map_page_size);
 
 void * map_list[list_size];
 int fd;
@@ -52,9 +53,8 @@ void * malloc(size_t size){
     if(size == 0) return NULL;
     unsigned map_page_size = size;
     if(size <= max_block_size){
+        if (size < 8) map_page_size = 7;
         //this simple algorithm rounds up the size to the next highest power of 2
-        //if its at 1 it'll stay at 1 so we have to round up first. 
-        if(size == 1) map_page_size = 2;
         map_page_size--;
         map_page_size |= map_page_size >> 1;
         map_page_size |= map_page_size >> 2;
@@ -63,7 +63,7 @@ void * malloc(size_t size){
         map_page_size |= map_page_size >> 16;
         map_page_size++;
         
-        int i = log(map_page_size)/log_2 - 1;
+        int i = log(map_page_size)/log_2 - 3;
         //if empty make it not empty
         if(map_list[i] == NULL){
             map_list[i] = new_map(map_page_size);
@@ -121,17 +121,15 @@ void free(void * ptr){
     //get beginning
     long temp = (long)ptr & ~0xfff;
     short* short_page_start = (short*)temp;
-    long* long_page_start = (long*)temp;
+    long* long_page_start = ptr;
     long_page_start--;
     int map_page_size;
 
     //if first bit is a 1 (big)
-    if(*short_page_start < 0){
+    if(*long_page_start < 0){
         //unmap the big
-        map_page_size = *short_page_start & 0x7fff;
-        if(munmap(short_page_start, map_page_size * page_size) == -1){
-            printf("oof\n");
-        }
+        map_page_size = *long_page_start & 0x7fffffffffffffff;
+        munmap(ptr, map_page_size);
         return;
     }else{
         //get the size of the page
@@ -152,30 +150,27 @@ void free(void * ptr){
 }
 
 void * calloc(size_t num, size_t size){
-    //just malloc and set bits to 0
     void * page = malloc(num * size);
     memset(page, 0, num * size);
     return page;
 }
 
 void * realloc(void * ptr, size_t size){
-    //if null then return a malloc'd pointer
     if(ptr == NULL) return malloc(size);
 
-    //get the start of the page
     long temp = (long)ptr & ~0xfff;
-    short* short_page_start = (short*)temp;
-    long* long_page_start = (long*)temp;
+    long* long_page_start = ptr;
+    long_page_start--;
     int old_length = 0;
 
-    // get the length
-    if(*short_page_start < 0){
-        old_length = (*short_page_start & 0x7fff);
+
+    if(*long_page_start < 0){
+        old_length = (*long_page_start & 0x7fffffffffffffff);
     }else{
         short* small_page = (short*)temp;
         old_length = *(small_page + 4);
     }
-    //make a new pointer, copy the data and then free that pointer
+    
     void* newptr = malloc(size);
     if(size < old_length)
         memcpy(newptr, ptr, size);
@@ -206,7 +201,6 @@ void* new_map(int map_page_size){
         *temp = (short)(temp + 1) & 0x0fff;
         temp++;
 
-        //setup free list
         while( page_size - ((long) (temp + map_page_size/2 + 1) & 0xfff) > map_page_size + 2){
             temp += map_page_size/2;
             *temp = (short)(temp + 1) & 0x0fff;
@@ -220,15 +214,27 @@ void* new_map(int map_page_size){
 }
 
 void * big_map(int size){
-    short* temp = NULL;
-    int total_maps = ((size + 2) % page_size) + 1;
-    void * map = mmap ( NULL , total_maps * page_size, PROT_READ | PROT_WRITE , MAP_PRIVATE | MAP_ANONYMOUS, -1 , 0);
-    temp = (short*)map;
+    long* temp = NULL;
+    void * map = mmap ( NULL , size + 8, PROT_READ | PROT_WRITE , MAP_PRIVATE | MAP_ANONYMOUS, -1 , 0);
+    temp = map;
 
-    //set size
-    *temp = total_maps;
-    *temp |= 0x8000;
+    *temp = size + 8;
+    *temp |= 0x8000000000000000;
     temp++;
     return temp;
 }
 
+
+int return_i(int map_page_size){
+    int i = 0;
+    if(map_page_size == 8) i = 0;
+    else if(map_page_size == 16) i = 1;
+    else if(map_page_size == 32) i = 2;
+    else if(map_page_size == 64) i = 3;
+    else if(map_page_size == 128) i = 4;
+    else if(map_page_size == 256) i = 5;
+    else if(map_page_size == 512) i = 6;
+    else if(map_page_size == 1024) i = 7;
+    else i = 8;
+    return i;
+}
